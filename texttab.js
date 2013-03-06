8004d1e206cfcb7b8a9ad3b74243a062f56d6560
Julien Zmiro <julien.zmiro@gmail.com>
2013-03-06 10:37:27 +0100

    Simple placeholder

--- texttab.js
+++ texttab.js
@@ -4,14 +4,27 @@ var textStored = null;
 var init = function () {
   $texteditor = document.getElementsByTagName('p')[0];
   textStored = localStorage.getItem('chrome-textTab-text');
-
   $texteditor.addEventListener("input", handleTextChange, false);
   loadText();
+  displayPlaceholder();
 };
 
 var handleTextChange = function () {
     var textToSave = $texteditor.innerHTML;
     localStorage.setItem('chrome-textTab-text', textToSave);
+    displayPlaceholder();
+};
+
+var displayPlaceholder = function () {
+  var text = $texteditor.textContent;
+  console.log(text);
+  if (text && text != null && text != " " && text != "\n" && text != "<br>") {
+    console.log("Content found");
+    delete($texteditor.dataset.placeholderempty);
+  } else {
+    console.log("empty");
+    $texteditor.dataset.placeholderempty = "true";
+  }
 };
 
 var loadText = function () {
